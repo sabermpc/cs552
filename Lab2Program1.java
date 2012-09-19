d757ba85ce70e57cc5bf5f3ecb7d4e7b104ae270
Rob <shiftf8@gmail.com>
2012-09-19 00:53:30 -0400

    Going home

--- Lab2Program1.java
+++ Lab2Program1.java
@@ -2,36 +2,38 @@ import java.util.Scanner;
 
 public class Lab2Program1 {
     public static void main(String[] args) {
-        int userInt;
-        int max, min, total;
-        float average;
-        int i, j; //counters
-        
-        for (max = 0, min = 1000, total = 0, i = 0, j = 0; i < 7; i++) {
-            while (inData >> strInput) {
-                stringstream(strInput) >> userInt;
-                
-                outData << userInt << " ";
-                j++;
-                
+        int userInt = -1;
+        int max = 0, min = 0, total = 0;
+        double average = 0.0;
+        int i = 0, pos = 0, neg = 0; //counters
+
+        while (userInt != 0) {
+            System.out.print("Input: ");
+
+            Scanner jint = new Scanner(System.in);
+            userInt = jint.nextInt();
+
+//            while (userInt != 0) {
+                i++;
+
                 //Checking for maximum.
                 if (userInt > max) max = userInt;
-                
+
                 //Checking for minimum.
                 if (userInt < min) min = userInt;
-                
-                //Totaling up line.
+
+                //Totaling up numbers.
                 total = total + userInt;
-                
-                //Average of the line.
+
+                //Average of the numbers.
                 average = total / 7;
-                if ((j % 7) == 0) {
+                if ((i % 7) == 0) {
                     average = 0;
                     max = 0;
                     min = 1000;
                     total = 0;
                 }
-            }
+//    		}
         }
     }
 }
