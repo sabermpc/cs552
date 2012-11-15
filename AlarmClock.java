fc0acf9e496793c40b4d2a7951139af8fee8f763
Rob <shiftf8@gmail.com>
2012-11-14 19:47:38 -0800

    Update AlarmClock.java
    not compiling

--- AlarmClock.java
+++ AlarmClock.java
@@ -1,17 +1,37 @@
 public class AlarmClock {
-    public AlarmClock() {}
+	private int hh, mm, ss;
+	private boolean pm, on;
+
+    public AlarmClock() {
+		hh = 0;
+		mm = 0;
+		ss = 0;
+		pm = false;
+	}
 	public AlarmClock(int hh, int mm, int ss) {
+
 	}
 	public AlarmClock(int hh, int mm, int ss, boolean pm) {
+
 	}
 	void setAlarm(int hh, int mm) {
 	}
 	void setAlarm(int hh, int mm, boolean pm) {
 	}
-	void turnAlarmOn() {}
-	void turnAlarmOff() {}
-	boolean isAlarmOn() {}
-	boolean isRinging() {}
+	void turnAlarmOn() {
+		on = true;
+	}
+	void turnAlarmOff() {
+		on = false;
+	}
+	boolean isAlarmOn() {
+		if (on) return true;
+		return false;
+	}
+	boolean isRinging() {
+		if ((hh == getHours())) return true;
+		return false;
+	}
 	boolean equals(AlarmClock otherAlarmClock) {}
 	public String toString() {}
 }
