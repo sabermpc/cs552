089cd948e58088fa2a88cc15174540eda726f895
Rob <shiftf8@gmail.com>
2012-11-20 08:34:18 -0500

    AlarmClock

--- AlarmClock.java
+++ AlarmClock.java
@@ -1,37 +1,105 @@
-public class AlarmClock {
-	private int hh, mm, ss;
-	private boolean pm, on;
+public class AlarmClock extends Clock {
+    private int h, h12, m;
+	private boolean pm, alarmOn;
 
-    public AlarmClock() {
-		hh = 0;
-		mm = 0;
-		ss = 0;
-		pm = false;
+	public AlarmClock() {
+		super();
+		defaultAlarm();
 	}
 	public AlarmClock(int hh, int mm, int ss) {
-
+		super(hh, mm, ss);
+		defaultAlarm();
 	}
 	public AlarmClock(int hh, int mm, int ss, boolean pm) {
-
+		super(hh, mm, ss, pm);
+		defaultAlarm();
 	}
 	void setAlarm(int hh, int mm) {
+		boolean err = false;
+
+		if ((hh < 0) || (hh > 23)) {
+			System.out.println("Invalid Hours entry.");
+			err = true;
+		}
+		if ((mm < 0) || (mm > 59)) {
+			System.out.println("Invalid Minutes entry.");
+			err = true;
+		}
+		if (err == true) {
+			defaultAlarm();
+			System.out.println("Alarm clock initialized to Midnight / Off.");
+		} else {
+			h = hh;
+			m = mm;
+			pm = this.isPM();
+		}
 	}
 	void setAlarm(int hh, int mm, boolean pm) {
+		boolean err = false;
+
+		if ((hh < 0) || (hh > 12)) {
+			System.out.println("Invalid Hours entry.");
+			err = true;
+		}
+		if ((mm < 0) || (mm > 59)) {
+			System.out.println("Invalid Minutes entry.");
+			err = true;
+		}
+		if (err == true) {
+			defaultAlarm();
+			System.out.println("Alarm clock initialized to Midnight / Off.");
+		} else {
+			h = hh;
+			m = mm;
+			this.pm = pm;
+		}
 	}
 	void turnAlarmOn() {
-		on = true;
+		alarmOn = true;
 	}
 	void turnAlarmOff() {
-		on = false;
+		alarmOn = false;
 	}
 	boolean isAlarmOn() {
-		if (on) return true;
+		if (alarmOn) return true;
 		return false;
 	}
 	boolean isRinging() {
-		if ((hh == getHours())) return true;
+		if ((h == super.getHours24()) && (m == super.getMinutes()) && isAlarmOn()) return true;
 		return false;
 	}
-	boolean equals(AlarmClock otherAlarmClock) {}
-	public String toString() {}
+	boolean equals(AlarmClock otherAlarmClock) {
+        if ((h == otherAlarmClock.getAlarmHours24()) && (m == otherAlarmClock.getAlarmMinutes()) && (isAlarmOn() == otherAlarmClock.isAlarmOn())) return true;
+        return false;
+	}
+	public String toString() {
+		String alarmIsOn, alarmIsRinging, pm;
+		
+		if (isAlarmOn()) alarmIsOn = "ON";
+		else alarmIsOn = "OFF";
+		if (isRinging()) alarmIsRinging = "RINGING";
+		else alarmIsRinging = "NOT RINGING";
+		if (h > 11) pm = "PM";
+		else pm = "AM";
+		
+		return "Clock " + super.toString() + "  Alarm [" + String.format("%02d:%02d / %02d:%02d ", getAlarmHours24(), getAlarmMinutes(), getAlarmHours12(), getAlarmMinutes()) + pm + " / " + alarmIsOn + " / " + alarmIsRinging + "]";
+	}
+
+	int getAlarmHours24() {
+		return h;
+	}
+	int getAlarmHours12() {
+		h12 = h;
+		if (h == 0) h12 = 12;
+		if (h > 12) h12 = h - 12;
+
+		return h12;
+	}
+	int getAlarmMinutes() {
+		return m;
+	}
+    private void defaultAlarm() {
+		setAlarm(0, 0);
+        turnAlarmOff();
+	}
 }
--- ClockTest.java
+++ ClockTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ClockTest {
     public static void main(String[] args) {
-    	Clock b = new Clock(23, 30, 30);
+/*        Clock b = new Clock(23, 30, 30);
 		Clock c = new Clock(11, 30, 30, true);
 //    	b.addSeconds(30);
 //		c.addSeconds(30);
@@ -32,5 +32,20 @@ public class ClockTest {
 		else System.out.println("AM");
 		System.out.println();
 		if (b.equals(c)) System.out.println("\nThese clocks are equal!!\n");
+*/
+		AlarmClock e = new AlarmClock(11, 59, 0, true);
+		AlarmClock f = new AlarmClock(23, 59, 0);
+		e.addHours(21);
+		e.turnAlarmOn();
+//		f.turnAlarmOn();
+		e.setAlarm(20, 59);
+		e.addMinutes(1);
+		f.setAlarm(0, 0);
+		f.addMinutes(1);
+		System.out.println(e);
+		System.out.println(f);
+//		System.out.printf("%02d:%02d PM %s - On %s - Ringing %s%n", e.getHours24(), e.getMinutes(), e.isPM(), e.isAlarmOn(), e.isRinging());
+//		System.out.printf("%02d:%02d PM %s - On %s - Ringing %s%n", f.getHours24(), f.getMinutes(), f.isPM(), f.isAlarmOn(), f.isRinging());
+		if (e.equals(f)) System.out.println("\nThese alarm clocks are set TO THE SAME TIME!!!\n");
 	}
 }
